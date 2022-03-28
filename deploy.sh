8b95321f65add0ddd57fab11f579be1c908e875e
shaqok <shaqok8@gmail.com>
2022-03-28 01:46:59 -0500

    rebuilding site Mon Mar 28 01:46:59 CDT 2022

---
+++ deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+# hugo -t <theme
+hugo -t hugo-theme-console
+
+# Go To Public folder, sub module commit
+cd public
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin main
+
+# Come Back up to the Project Root
+cd ..
+
+
+# blog repository Commit & Push
+git add .
+
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git push origin main
